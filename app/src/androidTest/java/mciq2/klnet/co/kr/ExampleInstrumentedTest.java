9e703ace5b51699b736eee6946237db165ce33eb
klnetCorp <118152553+klnetCorp@users.noreply.github.com>
2022-11-29 13:51:49 +0900

    gradle update

--- app/src/androidTest/java/mciq2/klnet/co/kr/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/mciq2/klnet/co/kr/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package mciq2.klnet.co.kr;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
